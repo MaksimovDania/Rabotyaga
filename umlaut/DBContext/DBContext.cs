d212281dc45e82ed907cc365d1fea571101caa09
Stoma Fedor <fedorstoma2@gmail.com>
2023-02-18 22:54:25 +0300

    Creating a database

---
+++ umlaut/DBContext/DBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DBContext
+{
+    public class DataBaseContext: DbContext
+    {
+        private const string Host = "localhost";
+        private const string Port = "5432";
+        private const string database = "Laba6db";
+        private const string Username = "postgres";
+        private const string Password = "Fndya21003";
+
+        public DbSet<Graduate> Graduates { get; set; }
+
+        public DataBaseContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql($"Host={Host};Port={Port};Database={database};Username={Username};Password={Password}");
+            //добавить константу
+        }
+    }
+}
---
+++ umlaut/DBModels/Graduate.cs
@@ -0,0 +1,32 @@
+﻿namespace DBModels
+{
+    public class Graduate
+    {
+        public int Id { get; set; }
+
+        public string? Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public string? Vacation { get; set; }
+
+        //добавить список специализаций
+
+        public int ExpectedSalary { get; set; }
+
+        public string? Location { get; set; }
+        //возможно потребуется отдельная сущность
+
+        public string? YearGraduation { get; set; }
+
+        public string? Faculty { get; set; }
+        //сами знаете
+
+        public int Experience { get; set; }
+
+        public string? ResumeLink { get; set; }
+
+
+
+    }
+}
