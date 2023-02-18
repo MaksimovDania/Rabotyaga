e7ff0a40808811fcb719cc192db597b0d07dede7
dima.ostrometskiy <ostrometskiy@internet.ru>
2023-02-18 22:00:48 +0300

    first experiments with parsing hh.ru

---
+++ umlaut/umlaut/Program.cs
@@ -0,0 +1,39 @@
+using umlaut;
+
+HHRuAPI api = new HHRuAPI();
+
+var hrefs = await api.GetProfileHrefs(1);
+
+foreach (var href in hrefs)
+{
+    Console.WriteLine(href);
+}
+Console.WriteLine(hrefs.Count());
+
+
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
